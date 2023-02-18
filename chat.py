e02349fe91a50f259cd79330e1c4debb46e7167d
himanushi <himanushi@users.noreply.github.com>
2023-02-18 13:12:37 +0900

    fix

--- chat.py
+++ chat.py
@@ -1,5 +1,5 @@
 import argparse
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM, AutoModelForCausalLM, AutoTokenizer
 
 parser = argparse.ArgumentParser()
 parser.add_argument("model_name", type=str, help="Name or path of the model to use for dialogue")
@@ -7,15 +7,25 @@ parser.add_argument("user_input", type=str, help="The first user input to start 
 args = parser.parse_args()
 
 model_name = args.model_name
-model = AutoModelForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+if "t5" in model_name:
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+elif "m2m100" in model_name:
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+else:
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
 while True:
     user_input = input("User: ") if not args.user_input else args.user_input
     if user_input == "exit":
         break
     input_ids = tokenizer.encode(user_input, return_tensors="pt")
-    output = model.generate(input_ids, max_length=1000, do_sample=True)
+    if "t5" in model_name or "m2m100" in model_name:
+        output = model.generate(input_ids, max_length=1000, do_sample=True)
+    else:
+        output = model.generate(input_ids, max_length=1000, do_sample=True, pad_token_id=tokenizer.eos_token_id)
     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
     print("Bot: " + output_text)
     args.user_input = None
